fa4478e32360e033028e96d8962028e542ad3291
Seth Watson <seth.watson1@gmail.com>
2018-10-24 15:11:29 -0600

    modified tableview for fake data

--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class SearchViewController: UIViewController {
+    var searchResults = [String]()
 
     //MARK: - Lifecycle Methods
     override func viewDidLoad() {
@@ -25,17 +26,31 @@ class SearchViewController: UIViewController {
 }
 
 extension SearchViewController: UISearchBarDelegate {
+    
+    // Add fake data to table view
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        print("The search text is '\(searchBar.text!)'")
+        searchResults = []
+        for i in 0...2 {
+            searchResults.append(String(format: "Fake Result %d for '%@'", i, searchBar.text!))
+        }
+        tableView.reloadData()
     }
 }
 
 extension SearchViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return searchResults.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        let cellIdentifier = "SearchResultCell"
+        
+        var cell:UITableViewCell! = tableView.dequeueReusableCell(withIdentifier: cellIdentifier)
+        if cell == nil {
+            cell = UITableViewCell(style: .default, reuseIdentifier: cellIdentifier)
+        }
+        
+        cell.textLabel!.text = searchResults[indexPath.row]
+        return cell
     }
 }
