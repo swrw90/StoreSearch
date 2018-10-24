9958c6b0f38406ec864d35054a46ee9a55473af9
Seth Watson <seth.watson1@gmail.com>
2018-10-24 14:15:46 -0600

    added tableView, searchBar and connected to outlets

--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -10,11 +10,17 @@ import UIKit
 
 class SearchViewController: UIViewController {
 
+    //MARK: - Lifecycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Add 64-point margin at the top - 20 points for the status bar and 44 points for the Search Bar
+        tableView.contentInset = UIEdgeInsets(top: 64, left: 0, bottom: 0, right: 0)
     }
 
 
+    //MARK: - Outlets
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
 }
 
