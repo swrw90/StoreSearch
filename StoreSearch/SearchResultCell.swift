90f80678473baa24fc24bf9a92c4a9e4e3ee7728
Seth Watson <seth.watson1@gmail.com>
2018-10-25 13:20:34 -0600

    added outlets to SearchResultCell and connected to nib

---
+++ StoreSearch/SearchResultCell.swift
@@ -0,0 +1,29 @@
+//
+//  SearchResultCell.swift
+//  StoreSearch
+//
+//  Created by Seth Watson on 10/25/18.
+//  Copyright © 2018 Seth Watson. All rights reserved.
+//
+
+import UIKit
+
+class SearchResultCell: UITableViewCell {
+    
+    //    MARK: - Outlets
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var artistNameLabel: UILabel!
+    @IBOutlet weak var artworkImageView: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
