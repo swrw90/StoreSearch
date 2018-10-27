3549df90444e4b994455fcc14dfdbf04edcf7f79
Seth Watson <seth.watson1@gmail.com>
2018-10-27 13:44:35 -0600

    added properties to SeachResult model

--- StoreSearch/SearchResult.swift
+++ StoreSearch/SearchResult.swift
@@ -18,12 +18,25 @@ class SearchResult:Codable, CustomStringConvertible {
     var artistName = ""
     var trackName = ""
     var kind = ""
+    var trackPrice = 0.0
+    var currency = ""
+    var imageSmall = ""
+    var imageLarge = ""
+    var storeURL = ""
+    var genre = ""
+    
+    enum CodingKeys: String, CodingKey {
+        case imageSmall = "artworkUrl60"
+        case imageLarge = "artworkUrl100"
+        case storeURL = "trackViewUrl"
+        case genre = "primaryGenreName"
+        case kind, artistName, trackName
+        case trackPrice, currency
+    }
     
     var description: String {
         return "Kind: \(kind) Name: \(name), Artist Name: \(artistName)\n"
     }
-    
-    
     
     var name:String {
         return trackName
