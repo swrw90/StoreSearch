edd2a118680e0aa705a1fb01cd75aba6f6bed451
Seth Watson <seth.watson1@gmail.com>
2018-10-31 20:21:52 -0600

    added detailView and segue from SearchViewController to detailPopUp

---
+++ DetailViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DetailViewController.swift
+//  StoreSearch
+//
+//  Created by Seth Watson on 10/31/18.
+//  Copyright © 2018 Seth Watson. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    // MARK:- Actions
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
