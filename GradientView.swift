7d1df74e71081faf7f3fa6fb9788524033611391
Seth Watson <seth.watson1@gmail.com>
2018-11-03 01:01:55 -0600

    added comments

--- GradientView.swift
+++ GradientView.swift
@@ -10,6 +10,7 @@ import Foundation
 import UIKit
 
 class GradientView: UIView {
+    // Set background color to transparent
     override init(frame: CGRect) {
         super.init(frame: frame)
         backgroundColor = UIColor.clear
@@ -20,19 +21,20 @@ class GradientView: UIView {
         backgroundColor = UIColor.clear
     }
     
+    // Draw gradient on top of transparent background
     override func draw(_ rect: CGRect) {
-        // 1
+        // 1st array represents two shades of black, 2nd array represents the percentages of where to place these colors in the gradient.
         let components: [CGFloat] = [ 0, 0, 0, 0.3, 0, 0, 0, 0.7 ]
         let locations: [CGFloat] = [ 0, 1 ]
-        // 2
+        // 2 Create gradient object
         let colorSpace = CGColorSpaceCreateDeviceRGB()
         let gradient = CGGradient(colorSpace: colorSpace, colorComponents: components, locations: locations, count: 2)
-        // 3
+        // 3 Define how big this gradient object should be
         let x = bounds.midX
         let y = bounds.midY
         let centerPoint = CGPoint(x: x, y : y)
         let radius = max(x, y)
-        // 4
+        // 4 Draws the gradient 
         let context = UIGraphicsGetCurrentContext()
         context?.drawRadialGradient(gradient!, startCenter: centerPoint, startRadius: 0, endCenter: centerPoint, endRadius: radius, options: .drawsAfterEndLocation)
     }
