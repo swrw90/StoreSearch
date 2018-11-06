b770bc54899fe10d4951f1ba3a43d176936a47c2
Seth Watson <seth.watson1@gmail.com>
2018-11-05 20:55:11 -0700

    added scrollView and pageLayout

--- LandscapeViewController.swift
+++ LandscapeViewController.swift
@@ -12,11 +12,19 @@ class LandscapeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        // Remove constraints from main view
+        view.removeConstraints(view.constraints)
+        view.translatesAutoresizingMaskIntoConstraints = true
+        // Remove constraints for page control
+        pageControl.removeConstraints(pageControl.constraints)
+        pageControl.translatesAutoresizingMaskIntoConstraints = true
+        // Remove constraints for scroll view
+        scrollView.removeConstraints(scrollView.constraints)
+        scrollView.translatesAutoresizingMaskIntoConstraints = true
     }
     
-
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var pageControl: UIPageControl!
     /*
     // MARK: - Navigation
 
