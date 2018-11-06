3cb478514144c5889e089aec210662c74e645371
Seth Watson <seth.watson1@gmail.com>
2018-11-05 22:28:16 -0700

    now there are buttons for each search result

--- LandscapeViewController.swift
+++ LandscapeViewController.swift
@@ -9,10 +9,10 @@
 import UIKit
 
 class LandscapeViewController: UIViewController {
-var searchResults = [SearchResult]()
+    var searchResults = [SearchResult]()
     private var firstTime = true
     
-//    MARK: - Lifecycle Methods
+    //    MARK: - Lifecycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         // Remove constraints from main view
@@ -40,7 +40,7 @@ var searchResults = [SearchResult]()
     }
     
     
-//    MARK: - Outlets
+    //    MARK: - Outlets
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var pageControl: UIPageControl!
     
@@ -64,11 +64,11 @@ var searchResults = [SearchResult]()
             itemWidth = 94
             marginX = 2
         case 667:
-                columnsPerPage = 7
-                itemWidth = 95
-                itemHeight = 98
-                marginX = 1
-                marginY = 29
+            columnsPerPage = 7
+            itemWidth = 95
+            itemHeight = 98
+            marginX = 1
+            marginY = 29
             
         case 736:
             columnsPerPage = 8
@@ -87,6 +87,38 @@ var searchResults = [SearchResult]()
         let buttonHeight: CGFloat = 82
         let paddingHorz = (itemWidth - buttonWidth)/2
         let paddingVert = (itemHeight - buttonHeight)/2
+        
+        // Add the buttons
+        var row = 0
+        var column = 0
+        var x = marginX
+        for (index, result) in searchResults.enumerated() {
+            // 1 Create the UIButton object
+            let button = UIButton(type: .system)
+            button.backgroundColor = UIColor.white
+            button.setTitle("\(index)", for: .normal)
+            // 2 Convert row to a CGFloat
+            button.frame = CGRect(x: x + paddingHorz, y: marginY + CGFloat(row)*itemHeight + paddingVert, width: buttonWidth, height: buttonHeight)
+            // 3 Add the new button object to the UIScrollView as a subview
+            scrollView.addSubview(button)
+            // 4 Use x and row variables to position buttons from top to bottom
+            row += 1
+            if row == rowsPerPage {
+                row = 0; x += itemWidth; column += 1
+                
+                if column == columnsPerPage {
+                    column = 0; x += marginX * 2
+                }
+            }
+        }
+        // Set scroll view content size
+        let buttonsPerPage = columnsPerPage * rowsPerPage
+        let numPages = 1 + (searchResults.count - 1) / buttonsPerPage
+        scrollView.contentSize = CGSize(
+            width: CGFloat(numPages) * viewWidth,
+            height: scrollView.bounds.size.height)
+        
+        print("Number of pages: \(numPages)")
     }
-
+    
 }
