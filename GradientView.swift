69f4ca34ec4f5edc01f4149b7d11c010959d51d8
Seth Watson <seth.watson1@gmail.com>
2018-11-05 19:55:52 -0700

    added animation

--- GradientView.swift
+++ GradientView.swift
@@ -14,11 +14,13 @@ class GradientView: UIView {
     override init(frame: CGRect) {
         super.init(frame: frame)
         backgroundColor = UIColor.clear
+        autoresizingMask = [.flexibleWidth , .flexibleHeight]
     }
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         backgroundColor = UIColor.clear
+        autoresizingMask = [.flexibleWidth , .flexibleHeight]
     }
     
     // Draw gradient on top of transparent background
