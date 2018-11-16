b319aada4831d3365b0bb3d39912e6f4707a153c
Seth Watson <seth.watson1@gmail.com>
2018-11-16 14:35:18 -0700

    modified app delegate for splitVC

--- StoreSearch/AppDelegate.swift
+++ StoreSearch/AppDelegate.swift
@@ -12,6 +12,22 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    var splitVC: UISplitViewController {
+        return window!.rootViewController as! UISplitViewController
+    }
+    
+    var searchVC: SearchViewController {
+        return splitVC.viewControllers.first as! SearchViewController
+    }
+    
+    var detailNavController: UINavigationController {
+        return splitVC.viewControllers.last as! UINavigationController
+    }
+    
+    var detailVC: DetailViewController {
+        return detailNavController.topViewController
+            as! DetailViewController
+    }
 
 
     // MARK:- Helper Methods
